db8e704fca646a57d5126d8f9a417dee21aee7be
darcinc <phoehne@penguinwerks.com>
2014-03-29 03:02:31 -0400

    Adding tests for features

--- achelous.js
+++ achelous.js
@@ -6,6 +6,36 @@ var _ = require('lodash');
 
 
 module.exports = function(p1, properties) {
+  var Entity = function() {
+    if (arguments.length === 2 && _.isString(arguments[0]) && _.isString(arguments[1])) {
+      this.rel = arguments[0];
+      this.href = arguments[1];
+    } else if(arguments.length === 1 && _.isObject(arguments[0])) {
+      if (arguments[0].class) { this.class = arguments[0].class; }
+      if (arguments[0].rel) { this.rel = arguments[0].rel; }
+      if (arguments[0].properties) { this.properties = arguments[0].properties; }
+      if (arguments[0].href) { this.href = arguments[0].href; }
+      if (arguments[0].links)  { this.links = arguments[0].links; }
+    }
+    var self = this;
+
+    this.addLink = function(rel, href) {
+      if (!self.links) {
+        self.links = [];
+      }
+      self.links.push({rel: rel, href: href});
+      return self;
+    }
+    return self;
+  };
+
+  var Action = function() {
+    if (arguments.length === 2 && _.isString(arguments[0]) && _.isString(arguments[1])) {
+      this.name = arguments[0];
+      this.href = arguments[1];
+    }
+  }
+
   if (_.isString(p1)) {
     this.class = [p1];
   } else if(_.isArray(p1)) {
@@ -16,9 +46,20 @@ module.exports = function(p1, properties) {
     this.properties = properties;
   }
 
-  this.addEntity = function(rel, href) {
+  this.addEntity = function() {
     if(!this.entities) { this.entities = []; }
-    this.entities.push({rel: rel, href: href});
+    if (arguments.length === 2 && _.isString(arguments[0]) &&_.isString(arguments[1])) {
+      this.entities.push(new Entity(arguments[0], arguments[1]));
+    } else if(arguments.length === 1 && _.isObject(arguments[0])) {
+      this.entities.push(new Entity(arguments[0]));
+    }
     return this;
+  }
+
+  this.addAction = function() {
+    if (!this.actions) { this.actions = []; }
+    if (arguments.length === 2 && _.isString(arguments[0]) && _.isString(arguments[1])) {
+      this.actions.push(new Action(arguments[0], arguments[1]));
+    }
   }
 }
--- test/achelous.js
+++ test/achelous.js
@@ -32,11 +32,69 @@ describe("achelous", function() {
   });
 
   describe("Entities", function() {
+    var ach;
+
+    beforeEach(function() {
+      ach = new Achelous("foo", {bar: "baz"});
+    });
+
     it("should allow adding entities with required fields", function() {
-      var ach = new Achelous("foo", {bar: "baz"});
       ach.addEntity("http://mydomain.com/relationship", "http://mydomain.com/foo/1234");
       ach.entities[0].rel.should.equal("http://mydomain.com/relationship");
       ach.entities[0].href.should.equal("http://mydomain.com/foo/1234");
+    });
+
+    it("should allow adding entities with a map of fields", function() {
+      ach.addEntity({
+        class: ['foo', 'bar'],
+        rel: ["http://foo.bar.com/myrelation"],
+        properties: {
+          propOne: "boo"
+        },
+        links: [
+          { rel: ["self"], href: "http//foo.bar.com/foo/1234" }
+        ]
+      });
+      ach.entities[0].class[0].should.equal('foo');
+      ach.entities[0].class[1].should.equal('bar');
+      ach.entities[0].rel[0].should.equal("http://foo.bar.com/myrelation");
+      ach.entities[0].properties.propOne.should.equal("boo")
+      ach.entities[0].links[0].rel[0].should.equal("self");
+      ach.entities[0].links[0].href.should.equal("http//foo.bar.com/foo/1234");
+    });
+
+    it("should allow you to add links to a an entity", function() {
+      ach.addEntity({
+        class: ['foo', 'bar'],
+        rel: ["http://foo.bar.com/myrelation"],
+        properties: {
+          propOne: "boo"
+        },
+        links: [
+          { rel: ["self"], href: "http//foo.bar.com/foo/1234" }
+        ]
+      });
+      ach.entities[0].addLink(["other", "foo"], "http://foo.bar.com/bar/1235");
+
+      ach.entities[0].links[1].rel[0].should.equal("other");
+      ach.entities[0].links[1].rel[1].should.equal("foo");
+      ach.entities[0].links[1].href.should.equal("http://foo.bar.com/bar/1235");
+    })
+  });
+
+  describe("Actions", function() {
+    var ach;
+
+    beforeEach(function() {
+      ach = new Achelous("foo", {bar: "baz"});
+    });
+
+    it("should allow us to add an action", function() {
+      ach.addAction("foo", "http://foo.bar.com/action/1");
+
+      ach.actions.length.should.equal(1);
+      ach.actions[0].name.should.equal("foo");
+      ach.actions[0].href.should.equal("http://foo.bar.com/action/1")
     });
   });
 });
